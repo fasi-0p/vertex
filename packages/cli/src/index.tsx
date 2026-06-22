29cbb2b989e07d42b982703ecc134c2ad864214b
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-06-22 22:51:23 +0530

    toast provider integrated

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -2,17 +2,20 @@ import { createCliRenderer, TextAttributes } from "@opentui/core";
 import { createRoot } from "@opentui/react";
 import {InputBar} from './components/input-bar'
 import {Header} from './components/header'
+import {ToastProvider} from './providers/toast'
 
 function App() {
   return (
-    <box alignItems="center" justifyContent="center" flexGrow={1}>
-      <box alignItems='center' justifyContent='center' backgroundColor='#0D0D12' height='100%' width='100%' gap={2}>
-        <Header/>
-        <box width='100%' maxWidth={78} paddingX={2}>
-          <InputBar onSubmit={()=>{}}/>
+    <ToastProvider>
+      <box alignItems="center" justifyContent="center" flexGrow={1}>
+        <box alignItems='center' justifyContent='center' backgroundColor='#0D0D12' height='100%' width='100%' gap={2}>
+          <Header/>
+          <box width='100%' maxWidth={78} paddingX={2}>
+            <InputBar onSubmit={()=>{}}/>
+          </box>
         </box>
       </box>
-    </box>
+    </ToastProvider>
   );
 }
 
