e2260a57adaa0179d029e788bd7f3cd643567410
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-06-28 21:08:42 +0530

    routing

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -1,39 +1,25 @@
 import { createCliRenderer, TextAttributes } from "@opentui/core";
 import { createRoot } from "@opentui/react";
-import {InputBar} from './components/input-bar'
-import {Header} from './components/header'
-import {ToastProvider} from './providers/toast'
-import {KeyboardLayerProvider} from './providers/keyboard-layer'
-import {DialogProvider} from './providers/dialog'
-import {ThemeProvider, useTheme} from './providers/theme'
+import { createMemoryRouter, RouterProvider } from "react-router";
+import { RootLayout } from "./layouts/root-layout";
+import { Home } from "./screens/home";
+import { NewSession } from "./screens/new-session";
+import { Session } from "./screens/session";
 
-function ThemedRoot(){
-  const {colors} = useTheme()
-  return(
-    <box alignItems="center" justifyContent="center" flexGrow={1}>
-            <box alignItems='center' justifyContent='center' backgroundColor='#0D0D12' height='100%' width='100%' gap={2}>
-              <Header/>
-              <box width='100%' maxWidth={78} paddingX={2}>
-                <InputBar onSubmit={()=>{}}/>
-              </box>
-            </box>
-          </box>
-  )
-}
+const router = createMemoryRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "sessions/new", element: <NewSession /> },
+      { path: "sessions/:id", element: <Session /> },
+    ]
+  }
+]);
 
 function App() {
-  return (
-    <ThemeProvider>
-      <KeyboardLayerProvider>
-          <DialogProvider>
-            <ToastProvider>
-              <ThemedRoot/>
-            </ToastProvider>
-          </DialogProvider>
-      </KeyboardLayerProvider>
-    </ThemeProvider>
-
-  );
+    return <RouterProvider router={router} />
 }
 
 const renderer = await createCliRenderer({
