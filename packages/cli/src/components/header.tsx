772b02ac6d959f594f17fac03a6020841aaf6857
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-08-19 17:57:48 +0530

    title dynamically changes with theme

--- packages/cli/src/components/header.tsx
+++ packages/cli/src/components/header.tsx
@@ -1,21 +1,49 @@
+// export function Header() {
+//   return (
+//     <box justifyContent="center" alignItems="center">
+//       <box flexDirection="row" justifyContent="center" gap={0.5} alignItems="center">
+//         {/* <ascii-font font="tiny" text="Verte" color="gray" />
+//         <ascii-font font="tiny" text="x" color="white" /> */}
+      
+//         <text>
+//             {`
+//             ██╗   ██╗███████╗██████╗ ████████╗███████╗██╗  ██╗
+//             ██║   ██║██╔════╝██╔══██╗╚══██╔══╝██╔════╝╚██╗██╔╝
+//             ██║   ██║█████╗  ██████╔╝   ██║   █████╗   ╚███╔╝
+//             ╚██╗ ██╔╝██╔══╝  ██╔══██╗   ██║   ██╔══╝   ██╔██╗
+//             ╚████╔╝ ███████╗██║  ██║   ██║   ███████╗██╔╝ ██╗
+//             ╚═══╝  ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+//             `}
+//         </text>
+      
+//       </box>
+//     </box>
+//   );
+// }
+
+import { useTheme } from "../providers/theme";
+
 export function Header() {
+  const { colors } = useTheme();
+
   return (
-    <box justifyContent="center" alignItems="center">
-      <box flexDirection="row" justifyContent="center" gap={0.5} alignItems="center">
-        <ascii-font font="tiny" text="Verte" color="gray" />
-        <ascii-font font="tiny" text="x" color="white" />
-      
-        {/* <text>
-            {`
-            ██╗   ██╗███████╗██████╗ ████████╗███████╗██╗  ██╗
-            ██║   ██║██╔════╝██╔══██╗╚══██╔══╝██╔════╝╚██╗██╔╝
-            ██║   ██║█████╗  ██████╔╝   ██║   █████╗   ╚███╔╝
-            ╚██╗ ██╔╝██╔══╝  ██╔══██╗   ██║   ██╔══╝   ██╔██╗
-            ╚████╔╝ ███████╗██║  ██║   ██║   ███████╗██╔╝ ██╗
-            ╚═══╝  ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
-            `}
-        </text> */}
-      
+    <box width="100%" justifyContent="center" alignItems="center">
+      <box
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+        gap={0.5}
+      >
+        <text fg={colors.primary}>
+          {`
+          
+██╗   ██╗███████╗██████╗ ████████╗███████╗██╗  ██╗
+██║   ██║██╔════╝██╔══██╗╚══██╔══╝██╔════╝╚██╗██╔╝
+██║   ██║█████╗  ██████╔╝   ██║   █████╗   ╚███╔╝
+╚██╗ ██╔╝██╔══╝  ██╔══██╗   ██║   ██╔══╝   ██╔██╗
+ ╚████╔╝ ███████╗██║  ██║   ██║   ███████╗██╔╝ ██╗
+  ╚═══╝  ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝`}
+        </text>
       </box>
     </box>
   );
